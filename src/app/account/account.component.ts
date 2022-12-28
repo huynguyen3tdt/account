4d0cb0c948a5f3e49b55e2246a181d760cc1be35
huynguyentdt <huynguyen3@cqtdt>
2022-12-28 15:57:30 +0700

    Formart code

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -9,57 +9,59 @@ import {
   ValidationErrors
 } from "@angular/forms";
 
-export function checkEmail(c: AbstractControl) {
-  const valueEmail = c.value.inputEmail;
+export function checkEmail(controls: AbstractControl) {
+  const valueEmail = controls.value.inputEmail;
   if (!valueEmail) {
     return {
-      emailNoLength: true,
+      emailNotEnoughLength: true,
       messageEmail: 'Nhập email'
     }
   }
   return null
 }
 
-export function matchPassword(c: AbstractControl) {
-  const v = c.value;
-  return (v.inputPassword === v.inputConfirm) ? null : {
+export function matchPassword(controls: AbstractControl) {
+  const value = controls.value;
+  return (value.inputPassword === value.inputConfirm) ? null : {
     passwordnotmatch: true,
     invalid: true,
     message: 'Các mật khẩu đã nhập không khớp. Hãy thử lại.'
   }
 }
 
-export function checkPassword(c: AbstractControl) {
-  const valuePassword = c.value.inputPassword;
-  console.log(valuePassword.length)
+export function checkPassword(controls: AbstractControl) {
+  const PASSWORD_MIN_LENGTH = 8;
+  const valuePassword = controls.value.inputPassword;
+
   if (!valuePassword) {
     return {
-      passNoLength: true,
+      passNotEnoughLength: true,
       invalid: true,
       message: 'Nhập mật khẩu'
     }
   }
-  if (valuePassword.length < 8) {
+
+  if (valuePassword.length < PASSWORD_MIN_LENGTH) {
     return {
       passLengthError: true,
       invalid: true,
       message: 'Mật khẩu phải nhiều hơn 8 ký tự'
     }
   }
-  const hasUpperCase = /[A-Z]+/.test(valuePassword);
+  const REX_UPPER_CASE = /[A-Z]+/.test(valuePassword);
 
-  const hasLowerCase = /[a-z]+/.test(valuePassword);
+  const REX_LOWER_CASE = /[a-z]+/.test(valuePassword);
 
-  const hasNumeric = /[0-9]+/.test(valuePassword);
+  const REX_NUMBER = /[0-9]+/.test(valuePassword);
 
-  const hasSpecialCharacter = /[!@#$%^&*()_+`,./;'{}]+/.test(valuePassword);
+  const REX_SPEACIAL_CHARACTER = /[!@#$%^&*()_+`,./;'{}]+/.test(valuePassword);
 
-  const passwordValid = (hasUpperCase && hasLowerCase) ||
-    (hasUpperCase && hasNumeric) ||
-    (hasUpperCase && hasSpecialCharacter) ||
-    (hasLowerCase && hasNumeric) ||
-    (hasNumeric && hasSpecialCharacter) ||
-    (hasLowerCase && hasSpecialCharacter)
+  const passwordValid = (REX_UPPER_CASE && REX_LOWER_CASE) ||
+    (REX_UPPER_CASE && REX_NUMBER) ||
+    (REX_UPPER_CASE && REX_SPEACIAL_CHARACTER) ||
+    (REX_LOWER_CASE && REX_NUMBER) ||
+    (REX_NUMBER && REX_SPEACIAL_CHARACTER) ||
+    (REX_LOWER_CASE && REX_SPEACIAL_CHARACTER)
 
   return !passwordValid ? {
     passwordStrength: true,
@@ -74,12 +76,12 @@ export function checkPassword(c: AbstractControl) {
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
-  rfRegister: FormGroup = new FormGroup({});
+  fgRegister: FormGroup = new FormGroup({});
   isSubmit = false;
   isConfirm = true;
 
   ngOnInit() {
-    this.rfRegister = new FormGroup({ //inputSurName thuộc vào rfRegister
+    this.fgRegister = new FormGroup({ //inputSurName thuộc vào fgRegister
         inputSurname: new FormControl('', {validators: [Validators.required], updateOn: "change"}),
         inputName: new FormControl('', Validators.required),
         inputEmail: new FormControl('', {
@@ -94,19 +96,11 @@ export class AccountComponent implements OnInit {
       {
         validators: [matchPassword, checkEmail, checkPassword]
       })
-
-    console.log(this.rfRegister)
-    console.log(this.rfRegister.statusChanges)
-    console.log(this.rfRegister.value)
-
-    this.rfRegister.get('inputSurname')?.valueChanges.subscribe(a => {
-      console.log(a);
-    })
   }
 
   onSubmit(rf: object) {
     this.isSubmit = true;
-    this.rfRegister.markAllAsTouched();
+    this.fgRegister.markAllAsTouched();
     console.log(rf)
   }
 
@@ -135,12 +129,14 @@ export class AccountComponent implements OnInit {
     type: "date"
   }
   contextMale = {
+    id: "male",
     controlName: "inputMale",
     displayName: "Giới tính",
     type: "radio",
     valueMale: "Male"
   }
   contextFeMale = {
+    id: "female",
     controlName: "inputMale",
     displayName: "Giới tính",
     type: "radio",
@@ -159,5 +155,3 @@ export class AccountComponent implements OnInit {
 
   }
 }
-
-// form control có các tham số (value, validate, validator)
