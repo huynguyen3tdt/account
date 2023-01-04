65452af5bd07e843410749916f4c2957bd3a509f
huynguyentdt <huynguyen3@cqtdt>
2023-01-04 14:03:00 +0700

    update review change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {TranslateService} from "@ngx-translate/core";
 export class AppComponent {
   title = 'registerForm';
   constructor(private translate: TranslateService) {
-    translate.addLangs(['vi','en','jp']);
+    translate.addLangs(['en','vi','jp']);
     translate.setDefaultLang("en");
   }
 }
